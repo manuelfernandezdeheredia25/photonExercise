5c69093d1b80346f73afa8ad02626e948a6f13f6
EXPERTO <manuelfernandezdeheredia.25@campuscamara.es>
2026-02-10 20:47:31 +0100

    start menu with tank options

---
+++ Assets/LocalAssets/Scripts/tankSelector.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TankSelector : MonoBehaviour
+{
+
+    public GameObject tankBaseButton;
+    public GameObject tankHeavyButton;
+    public GameObject tankFastButton;
+
+
+    public void OnClickTankBase()
+    {
+        // mark button as selected
+        // unmark other buttons as selected
+        // save selected option on variable
+
+    }
+
+    public void OnClickTankHeavy()
+    {
+
+    }
+
+    public void OnClickTankFast()
+    {
+
+    }
+}
